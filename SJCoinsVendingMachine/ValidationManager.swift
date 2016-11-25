748819add035f590d3d02cdc5e4980f423864b94
Oleg Pankiv <oleg.pankiv@gmail.com>
2016-11-25 12:57:51 +0200

    refactored Validation Manager

--- SJCoinsVendingMachine/ValidationManager.swift
+++ SJCoinsVendingMachine/ValidationManager.swift
@@ -19,36 +19,19 @@ class ValidationManager {
     
     class func validate(login: String) -> validationStatus {
         
-        if stringEmpty(login) {
-            return .isEmpty
-        } else {
-            return stringValidation(login)
-        }
+        return login.isEmpty ? .isEmpty : stringValidation(login)
     }
     
     class func validate(password: String) -> validationStatus {
         
-        if stringEmpty(password) {
-            return .isEmpty
-        } else {
-            return .success
-        }
-    }
-    
-    private class func stringEmpty(_ string: String) -> Bool {
-        
-        return string.isEmpty
+        return password.isEmpty ? .isEmpty : .success
     }
     
     private class func stringValidation(_ string: String) -> validationStatus {
         
         let regex = "[a-z^]*"
         let loginTest = NSPredicate.init(format: "SELF MATCHES %@", regex)
-        
-        if loginTest.evaluate(with: string) {
-            return .success
-        } else {
-            return .notAllowed
-        }
+    
+        return loginTest.evaluate(with: string) ? .success : .notAllowed
     }
 }
